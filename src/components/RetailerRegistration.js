66ce7ed547d2037dc50de21b92ad51282f195012
Aswin Haridas <aswinvharidas@gmail.com>
2022-12-05 17:16:33 +0530

    retailer signup connection incomplete error on post

--- src/components/RetailerRegistration.js
+++ src/components/RetailerRegistration.js
@@ -9,53 +9,69 @@ import { NotificationManager } from 'react-notifications';
 
 export default function RetailerRegistration()
 {
+    // Rohan
     const [ownerEmail,setOwnerEmail] = useState('');
     const [ownerPassword, setOwnerPassword] = useState('');
+
+    const [retailerName, setRetailerName] = useState('');
+    const [retailerEmail, setRetailerEmail] = useState('');
+    const [retailerPassword, setRetailerPassword] = useState('');
+    const [retailerPhone, setRetailerPhone] = useState('');
+    const [retailerAddress, setRetailerAddress] = useState('');
+    const [retailerPincode, setretailerPincode] = useState('');
+
     
-    const navigate =useNavigate();
-    const setOwEmail = (e) => {
-        setOwnerEmail(e.target.value);
+    const navigate = useNavigate();
+
+    const setRetName = (e) => {
+        setRetailerName(e.target.value);
     };
-    const setPass = (e) => {
-        setOwnerPassword(e.target.value);
+    const setRetEmail = (e) => {
+        setRetailerEmail(e.target.value);
+    };
+    const setRetPass = (e) => {
+        setRetailerPassword(e.target.value);
+    };
+    const setRetPhone = (e) => {
+        setRetailerPhone(e.target.value);
+    };
+    const setRetAddress = (e) => {
+        setRetailerAddress(e.target.value);
+    };
+    const setRetPin = (e) => {
+        setretailerPincode(e.target.value);
     };
 
 
 
     async function validate(e) {
         e.preventDefault();
-        const navigateOwner = (e) =>
+        const navigateRetailer = (e) =>
         {
             // 👇️ navigate to /
-            navigate("/owner");
+            navigate("/retailerlogin");
         };
         
         let data = {
-        ownerEmail: (ownerEmail),
-        ownerPassword: (ownerPassword)
+            retailerName: (retailerName),
+            retialerEmail: (retailerEmail),
+            retailerPassword: (retailerPassword),
+            retailerPhone: (retailerPhone),
+            retailerAddress: (retailerAddress),
+            retailerPincode: (retailerPincode)
         }
-        console.log(data.ownerEmail,data.ownerPassword);
+        console.log(data.retailerName,data.retailerEmail,data.retailerPhone,data.retailerAddress,data.retailerPincode);
         
 
-        await axios.post('http://localhost:4000/owner/signin',data)
+        await axios.post('http://localhost:4001/api/retailer/',data)
         .then( response => {console.log(response.data);
-        if (response.data==="valid"){
-            console.log("Welcome !")
+            console.log("Restration Success!");
             NotificationManager.success('Logged In Sucessfully', 'Successful!', 2000);
-            navigateOwner(e);
-            
-        }
-        else if (response.data === "incorrect password")
-        {
-            NotificationManager.error('Incorrect Password!', 'Error!');
-        }
-        else if(response.data==="no account found"){
-            NotificationManager.error('No Account Found!', 'Error!');
-        }
-        else{
-            NotificationManager.error('Please Verify Credentials!', 'Error!');
-        }
-        })
+            navigateRetailer(e);
+        }).catch(function (error) {
+            // handle error
+            console.log(error);
+          })
     };
     return (
         <div className='Container'>
@@ -71,23 +87,23 @@ export default function RetailerRegistration()
                     <div className='Auth-form-content'>
                         <h3 className='Auth-form-title'>Retailer Sign Up</h3>
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="text" id="ownerEmail" placeholder='Retailer Name' onChange={(e) => setOwEmail(e) }/>
+                            <input className='form-control mt-1 input' type="text" id="retailerName" placeholder='Retailer Name' onChange={(e) => setRetailerName(e) }/>
                         </div>
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="text" id="ownerEmail" placeholder='Email' onChange={(e) => setOwEmail(e) }/>
+                            <input className='form-control mt-1 input' type="text" id="retailerEmail" placeholder='Email' onChange={(e) => setRetailerEmail(e) }/>
                         </div>
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="password" id="ownerPassword" autoComplete="on" placeholder='Password' onChange={(e) => setPass(e) }/>
+                            <input className='form-control mt-1 input' type="password" id="retailerPassword" autoComplete="on" placeholder='Password' onChange={(e) => setRetailerPassword(e) }/>
                         </div>
                         
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="text" id="ownerEmail" placeholder='Phone' onChange={(e) => setOwEmail(e) }/>
+                            <input className='form-control mt-1 input' type="text" id="retailerPhone" placeholder='Phone' onChange={(e) => setRetailerPhone(e) }/>
                         </div>
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="text" id="ownerEmail" placeholder='Address' onChange={(e) => setOwEmail(e) }/>
+                            <input className='form-control mt-1 input' type="text" id="retailerAddress" placeholder='Address' onChange={(e) => setRetailerAddress(e) }/>
                         </div>
                         <div className='form-group mt-3'>
-                            <input className='form-control mt-1 input' type="text" id="ownerEmail" placeholder='Pincode' onChange={(e) => setOwEmail(e) }/>
+                            <input className='form-control mt-1 input' type="text" id="retailerPincode" placeholder='Pincode' onChange={(e) => setretailerPincode(e) }/>
                         </div>
                         <div className='d-grid gap-2 mt-3 justify-content-center align-content-center'>
                             <button  className='btn btn-primary ' id='btn' type="submit" >Submit</button>
